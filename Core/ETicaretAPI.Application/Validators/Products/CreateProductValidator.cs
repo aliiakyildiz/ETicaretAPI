7edfe7be532ee41bf6fbdf9a5eb8163ac1377ce1
Your NameAliAkyildiz <ali.akyildiz.94@gmail.com>
2022-05-06 15:15:13 +0300

    Added ValidationFilter

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,39 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen ürün adını boş geçmeyiniz.")
+                .MaximumLength(150)
+                .MinimumLength(5)
+                .WithMessage("Lütfen ürün adını 5 ile 150 karakter arasında giriniz.");
+
+            RuleFor(p => p.Stock)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen stok bilgisini boş geçmeyiniz")
+                .Must(s => s >= 0)
+                .WithMessage("Stok bilgisi Negatif olamaz!");
+
+            RuleFor(p => p.Price)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen fiyat bilgisini boş geçmeyiniz")
+                .Must(s => s >= 0)
+                .WithMessage("Fiyat bilgisi Negatif olamaz!");
+
+        }
+    }
+}
