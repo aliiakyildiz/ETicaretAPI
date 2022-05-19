c81020d4acae044acfd7954ad68e1de3dee200af
Your NameAliAkyildiz <ali.akyildiz.94@gmail.com>
2022-05-19 22:11:46 +0300

    Added file upload process

---
+++ Core/ETicaretAPI.Application/Services/IFileService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ETicaretAPI.Application.Services
+{
+    public interface IFileService
+    {
+        Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files);///path: wwwroot' dan sonraki yolu belirtecek.
+        Task<bool> CopyFileAsync(string path, IFormFile file);
+    }
+}
