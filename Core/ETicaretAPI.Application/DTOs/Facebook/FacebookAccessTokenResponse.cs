d07814b1cb3d36401fbadbbcc355332e19aa3c82
Your NameAliAkyildiz <ali.akyildiz.94@gmail.com>
2022-07-03 18:56:59 +0300

    Added Facebook Login

---
+++ Core/ETicaretAPI.Application/DTOs/Facebook/FacebookAccessTokenResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.DTOs.Facebook
+{
+    public class FacebookAccessTokenResponse
+    {
+        [JsonPropertyName("access-token")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("token-type")]
+        public string TokenType { get; set; }
+    }
+}
